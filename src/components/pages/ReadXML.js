0e303c059487b69be90fe8209fc082a7059d70dd
GotteNaresh <gotte.naresh@gmail.com>
2022-04-05 13:21:25 +0530

    read xml login updated

--- src/components/pages/ReadXML.js
+++ src/components/pages/ReadXML.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import {
-    useLocation,
-    useNavigate,
-    useParams
-  } from "react-router-dom";
-  
+import React from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+const loadXMLDoc = () => {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    // Request finished and response
+    // is ready and Status is "OK"
+    if (this.readyState == 4 && this.status == 200) {
+      empDetails(this);
+    }
+  };
+  // employee.xml is the external xml file
+  xmlhttp.open("GET", "./data/employee.xml", true);
+  xmlhttp.send();
+};
+
+const empDetails = (xml) => {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table = `<tr><th>Firstname</th><th>Lastname</th>
+        <th>Title</th><th>Division</th>
+        <th>Building</th><th>Room</th>
+      </tr>`;
+  var x = xmlDoc.getElementsByTagName("employee");
+
+  // Start to fetch the data by using TagName
+  for (i = 0; i < x.length; i++) {
+    table +=
+      "<tr><td>" +
+      x[i].getElementsByTagName("firstname")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("lastname")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("division")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("building")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("room")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+  }
+
+  // Print the xml data in table form
+  document.getElementById("id").innerHTML = table;
+};
+
 function ReadXML() {
-    let location = useLocation();
-      let navigate = useNavigate();
-      let {id} = useParams();
-      console.log("test", id);
+  let { id } = useParams();
+  console.log("test", id);
+  loadXMLDoc();
   return (
-    <div className='home'>
+    <div className="home">
       <h1>{id}</h1>
+      <table id="id"></table>
     </div>
   );
 }
